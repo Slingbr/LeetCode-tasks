f9338dbbd6d58cc448be5f0a7e360f64a5942574
eboateng <Eboatenggraham@gmail.com>
2025-04-16 23:39:20 +0100

    .

--- RemoveDuplicates.java
+++ RemoveDuplicates.java
@@ -3,11 +3,15 @@ public class RemoveDuplicates  {
         if (nums.length == 0) return 0;
         int k = 1;
 
-        
+
         for (int i = 1; i < nums.length; i++) {
             if (nums[i] != nums[k - 1]) {
                 nums[k] = nums[i];
                 k++;
 
+        }
     }
+
+    return k;
+
 }
