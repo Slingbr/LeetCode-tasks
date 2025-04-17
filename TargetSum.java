d4371cc6de182738aece5c7824145f769d6d239d
eboateng <Eboatenggraham@gmail.com>
2025-04-17 23:54:52 +0100

    .

--- TargetSum.java
+++ TargetSum.java
@@ -1,3 +1,13 @@
 public class TargetSum {
     public int[] twoSum(int[] nums, int target) {
         for (int i = 0; i < nums.length; i++) {
+                    int addNumber = target - nums[i];
+                    for (int j = 0; j < nums.length; j++) {
+                        if (i != j && nums[j] == addNumber) {
+                            return new int[] { i, j };
+                        }
+                    }
+                }
+                return new int[0]; // If no solution found (though the prompt says there always is one)
+            }
+        }
