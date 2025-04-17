ab486d6ff71af73b3d5473f2a49466ae11968d9a
eboateng <Eboatenggraham@gmail.com>
2025-04-18 00:41:45 +0100

    .

---
+++ TargetSumHash.java
@@ -0,0 +1,9 @@
+import java.util.HashMap;
+
+public class TargetSumHash {
+    public static int[] twoSum(int[] nums, int target) {
+        HashMap<Integer, Integer> map = new HashMap<>();
+
+        for (int i = 0; i < nums.length; i++) {
+            int complement = target - nums[i];
+
