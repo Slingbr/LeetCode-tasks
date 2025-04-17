08b59245314af88c1aa947b71a2d0fb7db9cdb19
eboateng <Eboatenggraham@gmail.com>
2025-04-17 23:54:21 +0100

    .

--- TargetSum.java
+++ TargetSum.java
@@ -1,3 +1,3 @@
-public int[] twoSum(int[] nums, int target) {
-    for (int i = 0; i < nums.length; i++) {
-        int addNumber = target - nums[i];
+public class TargetSum {
+    public int[] twoSum(int[] nums, int target) {
+        for (int i = 0; i < nums.length; i++) {
